3e89aa9c115531ca95524050609f24f69ee4d504
Destinyqjy <89529037+Destinyqjy@users.noreply.github.com>
2023-12-14 22:15:23 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,37 +1,31 @@
 import pygame
+from src.utils.scene_manager import SceneManager
 from src.scenes.gender_selection import GenderScene
+from src.scenes.male_roles_selection import MaleRolesScene
+from src.scenes.female_roles_selection import FemaleRolesScene
 
 # 游戏初始化
 pygame.init()
 window = pygame.display.set_mode(size=(800, 600))
 pygame.display.set_caption("Echoes of Nations")
 
-# 游戏全局变量
+# 加载游戏场景
+scene_manager = SceneManager()
+scene_manager.add_scene('gender', GenderScene(window, scene_manager))
+scene_manager.add_scene('male_roles', MaleRolesScene(window, scene_manager))
+scene_manager.add_scene('female_roles', FemaleRolesScene(window, scene_manager))
+
+scene_manager.switch_to('gender')
+
+# 开始游戏
 is_running = True
-
-# 创建性别选择界面的实例
-gender_selection = GenderScene(window)
-
-# 游戏主循环
 while is_running:
     # 事件处理
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
         else:
-            if gender_selection.is_active:  # 只有在界面激活时处理事件
-                gender_selection.handle_event(event)
-            if gender_selection.male_roles_scene.is_active:
-                gender_selection.male_roles_scene.handle_event(event)
-            if gender_selection.female_roles_scene.is_active:
-                gender_selection.female_roles_scene.handle_event(event)
+            scene_manager.update(event)
 
-    # 窗体绘制
-    if gender_selection.is_active:  # 只有在界面激活时绘制界面
-        gender_selection.draw()
-    if gender_selection.male_roles_scene.is_active:
-        gender_selection.male_roles_scene.draw()
-    if gender_selection.female_roles_scene.is_active:
-        gender_selection.female_roles_scene.draw()
-
+    scene_manager.draw()
     pygame.display.update()
